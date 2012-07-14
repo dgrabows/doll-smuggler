72420aa606092a60ed6b59629d199547a8047a4a
Dan Grabowski <dan.grabowski@gmail.com>
2012-07-13 23:23:44 -0400

    Initial project structure setup.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject smuggler "0.1.0-SNAPSHOT"
+  :description "Doll smuggler problem solution."
+  :url "https://github.com/dgrabows/doll-smuggler"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/smuggler/core_test.clj
@@ -0,0 +1,7 @@
+(ns smuggler.core-test
+  (:use clojure.test
+        smuggler.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
